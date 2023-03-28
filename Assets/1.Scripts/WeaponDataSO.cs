94bd5b0e770f268290d947f91c298cb4356c3d7f
kenneth <kc4338@nyu.edu>
2023-03-27 21:06:27 -0400

    Weapon Type ScriptableObject

---
+++ Assets/1.Scripts/WeaponDataSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName= "_WeaponDataSO", menuName = "ScriptableObject/ WeaponData")]
+public class WeaponDataSO : ScriptableObject
+{
+    public string weaponName;
+    public GameObject weaponPrefab;
+}
